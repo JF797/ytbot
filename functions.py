d770a4619394ba2f1d0f6d604560f062ffab61d0
sy <josiah@faulkner-harrington.co.uk>
2024-02-03 23:02:28 +0000

    Additions - Fixed logging to now work properly. This needed to be added to the start of the script file for some reason.
    - Moved all functions (mostly to do with file handling) to their own py file to keep main file clean.
    
    - Wanted to automate the creation process of the log files by doing retries but wasn't possible. Might take a look at this another time.

---
+++ functions.py
@@ -0,0 +1,38 @@
+import os
+import logging
+
+def checkingCookiesExists(directory):
+    if not os.path.exists(directory):
+        logging.warning(f'Could not find cookies.txt file at {directory}. Please ensure this is added')
+        return
+    else:
+        logging.info(f'Cookies.txt file found at {directory}')
+        return
+
+def checkFileExistsIfNoThenCreate(fileLocation):
+    if not os.path.exists(fileLocation):
+        logging.warning(f'Could not find file: {fileLocation}, creating now.')
+        os.system(f'touch {fileLocation}')
+        logging.info(f'File: {fileLocation} created.')
+    else:
+        logging.info(f'File: {fileLocation} already exists. Continuing')
+        return
+
+def checkDirExistsIfNoThenCreate(directory):
+    if not os.path.exists(directory):
+        logging.warning(f'Could not find directory {directory}, creating now.')
+        os.makedirs(directory, exist_ok=True)
+        logging.info(f'Directory: {directory} created.')
+    else:
+        logging.info(f'Directory {directory} already exists. Continuing')
+        return
+def getLinkName(videoLink):
+    return os.popen(f'yt-dlp --skip-download -e {videoLink}').read()
+
+
+def compileFullOutputFilePath(fileTimestamp, fileTitle, directory):
+    return f'{directory}/"{fileTimestamp}_{fileTitle}".mp4'
+
+def downloadVideo(videoLink, videoFullPath):
+    os.system(f'yt-dlp --output {videoFullPath} {videoLink}')
+    return
--- main.py
+++ main.py
@@ -1,98 +1,59 @@
 # imports
 import discord
-from datetime import datetime
-import logging
 import re
-import os
 import time
-
-# Configurations required for Discord
-intents = discord.Intents.all()
-client = discord.Client(intents=intents)
-# Set token value from token.txt file in same directory as project.
-with open("token", "r") as tokenFile:
-    TOKEN = tokenFile.read()
-
+from datetime import datetime
+from functions import *
 
 # Datetime settings for file structures and logging
 now = datetime.now()
 timestampNow = now.strftime("%Y%m%d_%H%M%S")
 
-# Directory configurations for local device. These will change depening on device running app
+# Directory configurations for local device. These will change depending on device running app
 directory = "/tmp/outputs"
 logLocation = directory + '/logs'
 logFilePath = (logLocation+now.strftime("/%Y%m%d.log"))
 cookiesLocation = (directory+"/cookies.txt")
 
+try:
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        datefmt='%m-%d %H:%M:%S',
+        filename=logFilePath,
+        filemode='a',
+    )
+    print(logFilePath)
+    logging.info('logging configured')
+except (FileNotFoundError):
+    logging.error(f'Log file not found in {logFilePath}. Re-run script again which should fix otherwise investigate.')
+
+# Check directories exist. If they don't then they will be created.
+checkDirExistsIfNoThenCreate(directory)
+checkDirExistsIfNoThenCreate(logLocation)
+checkFileExistsIfNoThenCreate(logFilePath)
+
+
+# Configurations required for Discord
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+# Set token value from token.txt file in same directory as project.
+with open("token", "r") as tokenFile:
+    TOKEN = tokenFile.read()
 
 # Constants / variables used in code
 linkFound = False
 
-# Configure logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-    datefmt='%m-%d %H:%M:%S',
-    filename=logFilePath,
-    filemode='a'
-)
-
-def checkingCookiesExists(directory):
-    if not os.path.exists(directory):
-        logging.warning(f'Could not find cookies.txt file at {directory}. Please ensure this is added')
-        return
-    else:
-        logging.info(f'Cookies.txt file found at {directory}')
-        return
-def checkDirExistsIfNoThenCreate(directory):
-    if not os.path.exists(directory):
-        logging.warning(f'Could not find directory {directory}, creating now.')
-        os.mkdir(directory)
-        logging.info(f'Directory: {directory} created.')
-    else:
-        logging.info(f'Directory {directory} already exists. Continuing')
-        return
-
-def checkFileExistsIfNoThenCreate(fileLocation):
-    if not os.path.exists(fileLocation):
-        logging.warning(f'Could not find file: {directory}, creating now.')
-        os.system(f'touch {fileLocation}')
-        logging.info(f'File: {fileLocation} created.')
-    else:
-        logging.info(f'File: {fileLocation} already exists. Continuing')
-        return
-
-
-def getLinkName(videoLink):
-    return os.popen(f'yt-dlp --skip-download -e {videoLink}').read()
-
-
-def compileFullOutputFilePath(fileTimestamp, fileTitle, directory):
-    return f'{directory}/"{fileTimestamp}_{fileTitle}".mp4'
-
-def downloadVideo(videoLink, videoFullPath):
-    os.system(f'yt-dlp --output {videoFullPath} {videoLink}')
-    return
 
 
 # Main code
-
-# Check main directory for downloads exists, if not then create it.
-checkDirExistsIfNoThenCreate(directory)
-
-# Check directory for logs exists, if not then create it.
-# Check if today's log file exists, if not then create it.
-checkDirExistsIfNoThenCreate(logLocation)
-checkFileExistsIfNoThenCreate(logFilePath)
-
 @client.event
 async def on_ready():
     logging.info(f"logged in as {client.user.name}")
     logging.info(f"with user ID: {client.user.id}")
 
-
-# logging
-@client.event 
+@client.event
 async def on_message(message):
     logging.info(f'message {message}')
     username = str(message.author)
